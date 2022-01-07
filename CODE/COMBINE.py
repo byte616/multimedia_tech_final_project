c242e8b8c9e00d2a91fef93698a89c7596ba0856
melamela <p500297@gmail.com>
2022-01-07 23:57:42 +0800

    add combine program

--- CODE/COMBINE.py
+++ CODE/COMBINE.py
@@ -1,6 +1,113 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+from PIL import Image, ImageOps, ImageFont, ImageDraw 
+import math
+from PIL import ImageFilter
+
+def add_border1(file_name):
+    image = Image.open(file_name)
+    image = ImageOps.expand(image, border=100, fill='white')
+    image.save("../example_result/deal.jpg")
+    return image.size
+
+def add_border2():
+    img = Image.open('../example_result/deal.jpg')
+    [a,b]=img.size
+    new_pic = Image.new('RGB', (img.size[0] , img.size[1] + int(a/2)), (255,255,255))
+    new_pic.paste(img, (3, 3))
+    return new_pic
+
+def add_text():
+    title_font = ImageFont.truetype('../FONT/AA.ttf',100)
+    title_text1="我不會去摘月亮"
+    title_text2="我要月亮奔我而來"
+    image_editable = ImageDraw.Draw(new_pic)
+    [n,m]=new_pic.size
+    x=(n-len(title_text1)*100)/2
+    y=b+50
+    image_editable.text((x,y), title_text1, (0,0,0),title_font)
+    x=(n-len(title_text2)*100)/2
+    image_editable.text((x,y+100), title_text2, (0,0,0),title_font)
+
+def softlightFilter(im):
+    [im_width, im_height] = im.size
+    pixels = im.load()
+    for i in range(im_width):
+        for j in range(im_height):
+            r, g, b = pixels[i, j]
+            r = 250 if r+30>250 else r+45
+            g = 250 if g+30>250 else g+45
+            b = 250 if b+30>250 else b+45
+            pixels[i,j] = (r, g, b)
+    im.save("../example_result/result1.jpg")
+
+def RGBtoGRAY(im):
+    im_width, im_height = im.size
+    pixels = im.load()
+    for i in range(im_width):
+        for j in range(im_height):
+            r, g, b = pixels[i, j]
+            gray = math.floor((r*30 + g*59 + b*11 + 50)/100)
+            pixels[i,j] = (gray, gray, gray)
+    im.save("../example_result/result2.jpg")
+
+def rmRED(im):
+    im_width, im_height = im.size
+    pixels = im.load()
+    for i in range(im_width):
+        for j in range(im_height):
+            r, g, b = pixels[i, j]
+            r = 0 if (g < 60 and b < 60) else r
+            g = 250 if g+30>250 else g+45
+            b = 250 if b+30>250 else b+45
+            pixels[i,j] = (r, g, b)
+    im.save("../example_result/result3.jpg")
+
+def inverted(im):
+    im_width, im_height = im.size
+    pixels = im.load()
+    for i in range(im_width):
+        for j in range(im_height):
+            r, g, b = pixels[i, j]
+            r = 255-r
+            g = 255-g
+            b = 255-b
+            pixels[i,j] = (r, g, b)
+    im.save("../example_result/result4.jpg")
+
+def edge(im):
+    im=im.filter(ImageFilter.FIND_EDGES)
+    im.save("../example_result/result5.jpg")
+
+## file name
+file_name="../image/42.jpg"
+## add border
+[a,b]=add_border1(file_name)
+new_pic=add_border2()
+## add text
+add_text()
+## save original pic
+new_pic.save('../example_result/result.jpg')
+print("SUCCESS #1")
+## softlight
+softlightFilter(new_pic)
+print("SUCCESS #2")
+## gray scale
+RGBtoGRAY(new_pic)
+print("SUCCESS #3")
+## rmRED
+rmRED(new_pic)
+print("SUCCESS #4")
+## invert
+inverted(new_pic)
+print("SUCCESS #5")
+## edge detection
+edge(new_pic)
+print("SUCCESS #6")
+
+#########################################################3
+
 window = tk.Tk()
 window.title('Window')
 align_mode = 'nswe'
@@ -72,7 +179,7 @@ image_main.grid(column=0, row=0, sticky=align_mode)
 # TITLE
 
 lbl_title1 = tk.Label(div2, text='假文青濾鏡', bg='black', fg='white')
-lbl_title2 = tk.Label(div2, text="CHOSE\nMODE", bg='black', fg='white')
+lbl_title2 = tk.Label(div2, text="CHOOSE\nMODE", bg='black', fg='white')
 
 lbl_title1.grid(column=0, row=0, sticky=align_mode)
 lbl_title2.grid(column=0, row=1, sticky=align_mode)
